623b4f6cab71b956427775971d91691cdce03bb5
MaquinoBSC <martinare.ipn@gmail.com>
2021-08-31 11:05:41 -0500

    Servicvios y container

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -1,18 +1,31 @@
 <?php
 
 namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LibraryController extends AbstractController
 {
+
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger= $logger;
+    }
     
     /**
      * @Route("/library/list", name="library_list")
      */
-    public function list(){
+    public function list(Request $request)
+    {
+        $title= $request->get('title', 'Alegria');
+        $this->logger->info('List action called');
         $response= new JsonResponse();
         $response->setData([
             'success'=> true,
@@ -25,6 +38,10 @@ class LibraryController extends AbstractController
                     'id' => 2,
                     'title' => 'El nombre del viento'
                 ],
+                [
+                    'id' => 3,
+                    'title' => $title
+                ]
             ]
         ]);
         return $response;
