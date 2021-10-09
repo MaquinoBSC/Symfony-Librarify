674662423dbd9ab6eaedc51d08dd744624b67dd5
MaquinoBSC <martinare.ipn@gmail.com>
2021-10-08 23:50:14 -0500

    Subir imagenes usando flysystem-bundle

--- src/Controller/Api/BooksController.php
+++ src/Controller/Api/BooksController.php
@@ -3,12 +3,14 @@
 namespace App\Controller\Api;
 
 use App\Entity\Book;
+use App\Form\Model\BookDto;
 use App\Form\Type\BookFormType;
 use App\Repository\BookRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Request;
+use League\Flysystem\FilesystemOperator;
 
 class BooksController extends AbstractFOSRestController
 {
@@ -26,13 +28,21 @@ class BooksController extends AbstractFOSRestController
      * @Rest\Post(path="/books")
      * @Rest\View(serializerGroups={"book"}, serializerEnableMaxDepthChecks=true)
      */
-    public function postAction(Request $request, EntityManagerInterface $em)
+    public function postAction(Request $request, EntityManagerInterface $em, FilesystemOperator $defaultStorage)
     {
-        $book= new Book();
-        $form= $this->createForm(BookFormType::class, $book);
+        $bookDto= new BookDto();
+        $form= $this->createForm(BookFormType::class, $bookDto);
         $form->handleRequest($request);
         
         if ($form->isSubmitted() && $form->isValid()) { 
+            $extension= explode('/', mime_content_type($bookDto->base64Image))[1];//Obtenemos la extension de la imagen
+            $data= explode(',', $bookDto->base64Image);//la codificacion de la imagen consta de 2 partes, informacion de la ruta y la codificacion
+            // estas dos partes estan separadas por una coma, hacemos las separacion
+            $filename= sprintf('/books/%s.%s', uniqid('book_', true), $extension);//Generar un nombre de fichero
+            $defaultStorage->write($filename, base64_decode($data[1]));//escribimos la imagen
+            $book= new Book();
+            $book->setTitle($bookDto->title);
+            $book->setImage($filename);
             $em->persist($book);
             $em->flush();
 
